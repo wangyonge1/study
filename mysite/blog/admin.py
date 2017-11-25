e64d12f3d87fce6b4c33b19e1daf75df97aab55e
wangye <wangyonge@sina.com>
2017-11-25 16:52:45 +0800

    django admin edit

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -4,4 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 import models
-admin.site.register(models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display=('title','content')
+    list_filter=('title',)
+admin.site.register(models.Article,ArticleAdmin)
